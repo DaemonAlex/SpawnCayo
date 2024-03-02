a7bbdbf2bb706324328c2e09d04e08438435cf24
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-01 19:32:17 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DeamonAlex "schtoop"'
+description 'Forces Cayo Perico Island and Mini Map to spawn in FiveM.'
+version '1.0.0'
+
+client_script 'client.lua'
